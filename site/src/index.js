76fea90f829f8c4233b88b7aa4f6d5b40c747a30
PierreDV <p.verteuil@gmail.com>
2016-07-13 15:57:51 -0400

    implemented react router in main index.js page

--- site/src/index.js
+++ site/src/index.js
@@ -2,13 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+import reduxThunk from 'redux-thunk';
+
 import  App from './components/app';
 import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
+const store = createStoreWithMiddleware(reducers);
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+  <Provider store={store}>
+    <Router history={browserHistory} >
+      <Route path="/" component={App}>
+        <IndexRoute component={App} /> 
+      </Route>
+    </Router>
   </Provider>
   , document.querySelector('.container'));
